9fab094192b15eeffc685bfd0325d6424d38dcf7
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-02-25 19:56:19 +0700

    menambahkan register dan logout member dan admin

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,9 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function () {
-    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('admin', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 Route::group([
