f7b8d9efa4998cc5b086dbb89354a984b591e40a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-02-25 19:57:46 +0700

    mengubah login admin dan menambahkan register dan login member dan logout admin dan member

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
         $credentials = request(['email', 'password']);
 
-        if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['Email or Password is wrong'], 401);
+        if (auth()->attempt($credentials)) {
+            $token = Auth::guard('api')->attempt($credentials);
+            return response()->json([
+                'success' => true,
+                'message' => 'Login Berhasil',
+                'token' => $token
+            ]);
         }
 
-        return $this->respondWithToken($token);
+        return response()->json([
+            'success' => false,
+            'message' => 'Email atau Password Salah'
+        ]);
     }
 
     protected function respondWithToken($token)
@@ -24,5 +41,87 @@ class AuthController extends Controller
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama_member' => 'required',
+            'provinsi' => 'required',
+            'kabupaten' => 'required',
+            'kecamatan' => 'required',
+            'detail_alamat' => 'required',
+            'no_hp' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|same:konfirmasi_password',
+            'konfirmasi_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                $validator->errors(), 422
+            ]);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($request->password);
+        unset($input['konfirmasi_password']);
+        $member = Member::create($input);
+
+        return response()->json([
+            'data' => $member
+        ]);
+    }
+
+    public function login_member(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                $validator->errors(), 422
+            ]);
+        }
+
+        $member = Member::where('email', $request->email)->first();
+
+        if ($member) {
+            if (Hash::check($request->password, $member->password)) {
+                $request->session()->regenerate();
+                return response()->json([
+                    'message' => 'Success',
+                    'data' => $member
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'failed',
+                    'data' => 'Password Salah'
+                ]);
+            }
+        } else {
+            return response()->json([
+                'message' => 'failed',
+                'data' => 'Email Salah'
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        return redirect('/login');
+        // auth()->logout();
+        // return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function logout_member()
+    {
+        Session::flush();
+
+        redirect('/login_member');
     }
 }
